e0b425c6b78654718ae572da0ab7150f09cf0d48
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2015-02-15 13:49:10 -0200

    [FEATURE] Rearranging code for better maintenance

--- nmap2acl.pl
+++ nmap2acl.pl
@@ -1,62 +1,63 @@
 #!/usr/bin/perl
 
-use v5.10.1;
+use strict;
 use warnings;
+
+#no warnings qw(experimental);
+use feature ':5.10';
 
 use Nmap::Parser;
 
-my $np = new Nmap::Parser;
-my $type = $ARGV[0];
+our $VERSION = 1.0;
+
+my $np     = Nmap::Parser->new;
+my $type   = $ARGV[0];
 my $infile = $ARGV[1];
 
 
-#GETTING SCAN INFORMATION
+#  GETTING SCAN INFORMATION
 $np->parsefile($infile);
 
-#DETECTING FIREWALL TYPE
-for ($type) {
-	when (/^cisco/)	{ $coment="!"; $mask="permit tcp host %s any eq %s\n"; $prefix="\n";}
-	when (/^pf/)	{ $coment="#"; $mask="pass quick on \$ext_if proto tcp from any to %s port %s\n"; 
-			  $prefix = "# This is an example of firewall configuration. It should be changed to meet your needs.
-# Configuration of interfaces
-ext_if=COMPLETE HERE
-int_if=COMPLETE HERE
+#  DETECTING FIREWALL TYPE
+my $mask;
+my $prefix;
+my $comment;
 
-set skip on lo
-
-# Blocks everything on the external interface (below the nmap2acl allow legitimate traffic)
-block in on \$ext_if all
-block out on \$ext_if all
-
-# No blocks on the internal interface
-pass in on \$int_if all
-pass out on \$int_if all\n\n";}
-
-	when (/^iptables/)	{ $coment="#"; $mask="iptables -A INPUT -p tcp -s %s --sport %s -j ACCEPT\n";
-			  $prefix = "iptables -F
-
-# Skip conectons established and related
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# Setting policy to DROP by default (below the nmap2acl allow legitimate traffic)
-iptables -A INPUT -p tcp -j DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT DROP\n\n";}
-	default		{ $coment="!"; $mask="permit tcp host %s any eq %s\n"; $prefix="\n"; }
+given ($type) {
+    when (/^cisco/smx) {
+        $comment = "\nremark %s\n";
+        $mask    = "permit tcp host %s any eq %s\n";
+        $prefix  = "\n";
+    }
+    when (/^pf/smx) {
+        $comment = "\n# %s\n";
+        $mask    = "pass quick on \$ext_if proto tcp from any to %s port %s\n";
+        $prefix  = "\n";
     }
 
-printf ("%s",$prefix);
-for my $host ($np->all_hosts()){
-    for my $port ($host->tcp_ports()){
-        my $service = $host->tcp_service($port);
-        my $os = $host->os_sig;
-        # imprime o servico tambem
-	#print "permit tcp host ".$host->ipv4_addr()." any eq ".$port."|".$service->name."\n";
-        #print "permit tcp host ".$host->ipv4_addr()." any eq ".$port."\n";
-        printf($mask,$host->ipv4_addr(),$port);
-	}
+    when (/^iptables/smx) {
+        $comment = "\n# %s \n";
+        $mask    = "iptables -A INPUT -p tcp -s %s --sport %s -j ACCEPT\n";
+        $prefix  = "\n";
+    }
+
+    default {
+        $comment = "\nremark %s\n";
+        $mask    = "permit tcp host %s any eq %s\n";
+        $prefix  = "\n";
+    }
 }
 
+printf '%s', $prefix;
+
+for my $host ($np->all_hosts()) {
+    printf $comment, $host->addr();
+
+    for my $port ($host->tcp_ports()) {
+        my $os = $host->os_sig;
+
+        printf $mask, $host->ipv4_addr(), $port;
+    }
+}
 
 
